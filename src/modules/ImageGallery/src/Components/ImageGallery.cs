5db5fc767839630873a311f5c35acdce45cd6716
Rafael Caro <rafael.caro@plainconcepts.com>
2022-08-29 15:06:14 +0200

    feat: Image Gallery reacting to button clicks

--- src/modules/ImageGallery/src/Components/ImageGallery.cs
+++ src/modules/ImageGallery/src/Components/ImageGallery.cs
@@ -1,11 +1,50 @@
 ﻿using Evergine.Framework;
+using Evergine.MRTK.SDK.Features.UX.Components.PressableButtons;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Xrv.ImageGallery.Components
 {
     public class ImageGallery : Component
     {
+        //[BindComponent(source: BindComponentSource.Children, tag: "PART_image_gallery_next")]
+        private PressableButton nextButton;
+        //[BindComponent(source: BindComponentSource.Children, tag: "PART_image_gallery_previous")]
+        private PressableButton previousButton;
+
+        [BindEntity(source:BindEntitySource.Children, tag: "PART_image_gallery_next")]
+        private Entity nextButtonEntity;
+        
+        [BindEntity(source:BindEntitySource.Children, tag: "PART_image_gallery_previous")]
+        private Entity previousButtonEntity;
+
+        protected override bool OnAttached()
+        {
+            this.nextButton = nextButtonEntity.FindComponentInChildren<PressableButton>();
+            this.previousButton = previousButtonEntity.FindComponentInChildren<PressableButton>();
+
+            this.nextButton.ButtonPressed += this.onNextButtonPressed;
+            this.previousButton.ButtonPressed += this.onPreviousButtonPressed;
+            return base.OnAttached();
+        }
+
+        protected override void OnDetach()
+        {
+            this.nextButton.ButtonPressed -= this.onNextButtonPressed;
+            this.previousButton.ButtonPressed -= this.onPreviousButtonPressed;
+            base.OnDetach();
+        }
+
+        public void onNextButtonPressed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("NEXT");
+        }
+
+        public void onPreviousButtonPressed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("PREVIOUS");
+        }
     }
 }
