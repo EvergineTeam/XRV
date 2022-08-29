1ad9e0ac6044df60a163836f9a494659c5f6bf48
Rafael Caro <rafael.caro@plainconcepts.com>
2022-08-29 14:40:10 +0200

    feat: Buttons, slider, and basics of imagegallery component

---
+++ src/modules/ImageGallery/src/Components/ImageGallery.cs
@@ -0,0 +1,11 @@
+﻿using Evergine.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xrv.ImageGallery.Components
+{
+    public class ImageGallery : Component
+    {
+    }
+}
