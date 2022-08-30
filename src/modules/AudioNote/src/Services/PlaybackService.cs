d5a9c19feebeb3eff29196f2103f45856f2de9c1
Manuel Gomar <illanos@gmail.com>
2022-08-30 12:30:44 +0200

    added style cop

--- src/modules/AudioNote/src/Services/PlaybackService.cs
+++ src/modules/AudioNote/src/Services/PlaybackService.cs
@@ -1,4 +1,6 @@
-﻿using Evergine.Common.Audio;
+﻿// Copyright © Plain Concepts S.L.U. All rights reserved. Use is subject to license terms.
+
+using Evergine.Common.Audio;
 using Evergine.Framework;
 using Evergine.Framework.Services;
 using System;
@@ -7,37 +9,75 @@ using System.Threading.Tasks;
 
 namespace Xrv.AudioNote.Services
 {
+    /// <summary>
+    /// Playback updatable service.
+    /// </summary>
     public class PlaybackService : UpdatableService
     {
+        /// <summary>
+        /// audio device.
+        /// </summary>
         [BindService]
         protected AudioDevice audioDevice;
 
+        /// <summary>
+        /// assets Service.
+        /// </summary>
         [BindService]
         protected AssetsService assetsService;
 
+        /// <summary>
+        /// audio Source.
+        /// </summary>
         protected AudioSource audioSource;
 
         private AudioBuffer buffer;
         private WaveFormat format;
 
-        public event EventHandler<TimeSpan> CurrentPosition;
-
-        public event EventHandler AudioEnd;
-
-        public bool IsPlaying { get; protected set; }
-
-        public TimeSpan Position { get; protected set; } = TimeSpan.Zero;
-
-        public TimeSpan Duration { get; protected set; } = TimeSpan.Zero;
-
-        public float Volume { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlaybackService"/> class.
+        /// </summary>
         public PlaybackService()
         {
             // TODO this is config of sample
             this.format = new WaveFormat(true, sampleRate: 22050, encoding: WaveFormatEncodings.PCM8);
         }
 
+        /// <summary>
+        /// On position changed.
+        /// </summary>
+        public event EventHandler<TimeSpan> OnPositionChanged;
+
+        /// <summary>
+        /// On audio end
+        /// </summary>
+        public event EventHandler OnAudioEnd;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether isPlaying.
+        /// </summary>
+        public bool IsPlaying { get; protected set; }
+
+        /// <summary>
+        /// Gets or sets Position.
+        /// </summary>
+        public TimeSpan Position { get; protected set; } = TimeSpan.Zero;
+
+        /// <summary>
+        /// Gets or sets Duration.
+        /// </summary>
+        public TimeSpan Duration { get; protected set; } = TimeSpan.Zero;
+
+        /// <summary>
+        /// Gets or sets Volume.
+        /// </summary>
+        public float Volume { get; set; } = 0.5f;
+
+        /// <summary>
+        /// Loads an audio stream.
+        /// </summary>
+        /// <param name="stream">Audio strram</param>
+        /// <returns>A <see cref="Task{TResult}"/> true if audio is loaded.</returns>
         public async Task<bool> Load(Stream stream)
         {
             try
@@ -45,18 +85,19 @@ namespace Xrv.AudioNote.Services
                 if (this.audioDevice != null && this.audioSource == null)
                 {
                     this.audioSource = this.audioDevice.CreateAudioSource(this.format);
-                    this.audioSource.Volume = 0.1f;
+                    this.audioSource.Volume = this.Volume;
                 }
 
                 this.audioSource.Stop();
                 this.audioSource.FlushBuffers();
+                await Task.Delay(1); // TODO do real load from audio
                 ////this.buffer = this.audioDevice.CreateAudioBuffer();
                 ////await this.buffer.FillAsync(stream, (int)stream.Length, this.format);
 
-                this.buffer = assetsService.Load<AudioBuffer>(AudioNoteResourceIDs.Audio.Sample);
+                this.buffer = this.assetsService.Load<AudioBuffer>(AudioNoteResourceIDs.Audio.Sample);
                 this.Duration = this.buffer.Duration;
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 // TODO log exception
                 return false;
@@ -65,20 +106,9 @@ namespace Xrv.AudioNote.Services
             return true;
         }
 
-        private void AudioSource_BufferEnded(object sender, AudioBufferEventArgs e)
-        {
-            this.audioSource.BufferEnded -= this.AudioSource_BufferEnded;
-            this.AudioEnd?.Invoke(this, EventArgs.Empty);
-        }
-
-        public void Reset()
-        {
-            this.buffer = null;
-            this.audioSource.Stop();
-            this.IsPlaying = false;
-            this.Position = TimeSpan.Zero;
-        }
-
+        /// <summary>
+        /// Play audio.
+        /// </summary>
         public void Play()
         {
             this.audioSource.EnqueueBuffer(this.buffer);
@@ -87,23 +117,33 @@ namespace Xrv.AudioNote.Services
             this.IsPlaying = true;
         }
 
+        /// <summary>
+        /// Stop audio.
+        /// </summary>
         public void Stop()
         {
             this.audioSource.Stop();
             this.IsPlaying = false;
             this.Position = TimeSpan.Zero;
-            this.CurrentPosition?.Invoke(this, this.Position);
-            this.AudioEnd?.Invoke(this, EventArgs.Empty);
+            this.OnPositionChanged?.Invoke(this, this.Position);
+            this.OnAudioEnd?.Invoke(this, EventArgs.Empty);
             this.audioSource.BufferEnded -= this.AudioSource_BufferEnded;
         }
 
+        /// <inheritdoc/>
         public override void Update(TimeSpan gameTime)
         {
             if (this.IsPlaying)
             {
                 this.Position = this.audioSource.PlayPosition;
-                this.CurrentPosition?.Invoke(this, this.Position);
+                this.OnPositionChanged?.Invoke(this, this.Position);
             }
+        }
+
+        private void AudioSource_BufferEnded(object sender, AudioBufferEventArgs e)
+        {
+            this.audioSource.BufferEnded -= this.AudioSource_BufferEnded;
+            this.OnAudioEnd?.Invoke(this, EventArgs.Empty);
         }
     }
 }
--- src/modules/AudioNote/src/Services/RecordingService.cs
+++ src/modules/AudioNote/src/Services/RecordingService.cs
@@ -1,29 +1,46 @@
-﻿using Evergine.Framework.Services;
+﻿// Copyright © Plain Concepts S.L.U. All rights reserved. Use is subject to license terms.
+
+using Evergine.Framework.Services;
 using System;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace Xrv.AudioNote.Services
 {
+    /// <summary>
+    /// Recording updatable service.
+    /// </summary>
     public class RecordingService : UpdatableService
     {
+        /// <summary>
+        /// On recording time changed
+        /// </summary>
+        public event EventHandler<TimeSpan> OnRecordingTime;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether indicates if is recording.
+        /// </summary>
         public bool IsRecording { get; protected set; }
 
+        /// <summary>
+        /// Gets or sets current Recording Time.
+        /// </summary>
         public TimeSpan RecordingTime { get; protected set; }
 
-        public Stream BufferStream { get; protected set; }
-
-        public event EventHandler<TimeSpan> RecordingTimeChanged;
-
+        /// <inheritdoc/>
         public override void Update(TimeSpan gameTime)
         {
             if (this.IsRecording)
             {
                 this.RecordingTime += gameTime;
-                this.RecordingTimeChanged?.Invoke(this, this.RecordingTime);
+                this.OnRecordingTime?.Invoke(this, this.RecordingTime);
             }
         }
 
+        /// <summary>
+        /// Start recording async.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> true if ok.</returns>
         public async Task<bool> StartRecordingAsync()
         {
             this.IsRecording = true;
@@ -32,6 +49,10 @@ namespace Xrv.AudioNote.Services
             return true;
         }
 
+        /// <summary>
+        /// Stop recording async.
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> Stream with data recorded.</returns>
         public async Task<Stream> StopRecordingAsync()
         {
             this.IsRecording = false;
