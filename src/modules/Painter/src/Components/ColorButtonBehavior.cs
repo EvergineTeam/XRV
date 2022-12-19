1100191a85dd1bbfc7648e29075dc97cef837ec4
Rafael Caro <rafael.caro@plainconcepts.com>
2022-12-19 18:14:51 +0100

    fix: release

--- src/modules/Painter/src/Components/ColorButtonBehavior.cs
+++ src/modules/Painter/src/Components/ColorButtonBehavior.cs
@@ -24,9 +24,6 @@ namespace Xrv.Painter.Components
         [BindEntity(isRequired: false, source: BindEntitySource.ChildrenSkipOwner, tag: "Hover", isRecursive: false)]
         private readonly Entity hoverEntity = null;
 
-        [BindComponent]
-        private ToggleButton toggleButton = null;
-
         [BindComponent(isRequired: false)]
         private XrvPressableButtonLookAndFeel lookAndFeel = null;
 
