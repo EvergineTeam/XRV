ff23467be536d3d9e67b3a9188593e4c4235bc58
Sergio Escalada <sescalada@plainconcepts.com>
2023-05-05 20:29:20 +0200

    [Networking][Participants] Make avatar parts factory to allow users return

--- src/core/Evergine.Xrv.Core/Networking/Participants/AvatarPartsFactory.cs
+++ src/core/Evergine.Xrv.Core/Networking/Participants/AvatarPartsFactory.cs
@@ -32,7 +32,13 @@ namespace Evergine.Xrv.Core.Networking.Participants
             this.AssetsService = assetsService;
         }
 
-        internal virtual async Task<Entity> InstantiateElementAsync(ParticipantInfo participant, TrackedElement element)
+        /// <summary>
+        /// Gets entity to be loaded as participant avatar part, defined by tracked element.
+        /// </summary>
+        /// <param name="participant">Participant.</param>
+        /// <param name="element">Tracked element.</param>
+        /// <returns>Entity to be shown as tracked body part for participant avatar.</returns>
+        public virtual async Task<Entity> InstantiateElementAsync(ParticipantInfo participant, TrackedElement element)
         {
             if (participant.IsLocalClient)
             {
@@ -43,10 +49,35 @@ namespace Evergine.Xrv.Core.Networking.Participants
 
             var prefabId = this.GetPrefabIdByElement(participant, element);
             var entity = await this.CreateEntityFromPrefabAsync(prefabId).ConfigureAwait(false);
+            this.AfterEntityInstantiation(entity, participant, element);
 
+            return entity;
+        }
+
+        /// <summary>
+        /// Applies some customizations after entity has been instantiated. If you are extending this class,
+        /// maybe you should override default behavior of this method.
+        /// By default:
+        /// - It looks for a <see cref="AvatarDisplayName" /> component in head to set avatar name.
+        /// - For controllers, it adds a color indicator (this is temporary done by code, until
+        /// we have a definitive model).
+        /// - It searchs <see cref="AvatarTintColor" /> instances in depth to apply avatar tint color.
+        /// </summary>
+        /// <param name="entity">Avatar part entity.</param>
+        /// <param name="participant">Participant.</param>
+        /// <param name="element">Tracked element.</param>
+        protected virtual void AfterEntityInstantiation(
+            Entity entity,
+            ParticipantInfo participant,
+            TrackedElement element)
+        {
             if (element == TrackedElement.Head)
             {
-                entity.FindComponentInChildren<AvatarDisplayName>().Nickname = participant.Nickname;
+                var displayName = entity.FindComponentInChildren<AvatarDisplayName>();
+                if (displayName != null)
+                {
+                    displayName.Nickname = participant.Nickname;
+                }
             }
 
             // 3D elements tinting
@@ -69,13 +100,35 @@ namespace Evergine.Xrv.Core.Networking.Participants
                 entity.AddChild(colorMarkEntity);
             }
 
-            var tintComponents = entity.FindComponentsInChildren<AvatarTintColor>();
-            foreach (var tint in tintComponents)
-            {
-                tint.TintColor = participant.AvatarColor;
-            }
+            this.ApplyTintColor(entity, participant);
+        }
 
-            return entity;
+        /// <summary>
+        /// Gets prefab ID for given participant and tracked element type.
+        /// </summary>
+        /// <param name="participant">Participant.</param>
+        /// <param name="element">Tracked element.</param>
+        /// <returns>Prefab ID.</returns>
+        /// <exception cref="ArgumentOutOfRangeException">Raised for not supported elements.</exception>
+        protected virtual Guid GetPrefabIdByElement(ParticipantInfo participant, TrackedElement element)
+        {
+            switch (element)
+            {
+                case TrackedElement.Head:
+                    return participant.DeviceInfo?.IsHoloLens() ?? false
+                        ? CoreResourcesIDs.Prefabs.Networking.Participants.HoloLensHead_weprefab
+                        : CoreResourcesIDs.Prefabs.Networking.Participants.DefaultHead_weprefab;
+                case TrackedElement.LeftHand:
+                    return CoreResourcesIDs.Prefabs.Networking.Participants.leftPalm_weprefab;
+                case TrackedElement.RightHand:
+                    return CoreResourcesIDs.Prefabs.Networking.Participants.rightPalm_weprefab;
+                case TrackedElement.LeftController:
+                    return MRTKResourceIDs.Prefabs.DefaultLeftController_weprefab;
+                case TrackedElement.RightController:
+                    return MRTKResourceIDs.Prefabs.DefaultRightController_weprefab;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(element));
+            }
         }
 
         /// <summary>
@@ -104,30 +157,17 @@ namespace Evergine.Xrv.Core.Networking.Participants
         }
 
         /// <summary>
-        /// Gets prefab ID for given participant and tracked element type.
+        /// Searchs for <see cref="AvatarTintColor"/> components in hierarchy to apply
+        /// avatar tint color.
         /// </summary>
+        /// <param name="entity">Avatar part entity.</param>
         /// <param name="participant">Participant.</param>
-        /// <param name="element">Tracked element.</param>
-        /// <returns>Prefab ID.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Raised for not supported elements.</exception>
-        protected virtual Guid GetPrefabIdByElement(ParticipantInfo participant, TrackedElement element)
+        protected void ApplyTintColor(Entity entity, ParticipantInfo participant)
         {
-            switch (element)
+            var tintComponents = entity.FindComponentsInChildren<AvatarTintColor>();
+            foreach (var tint in tintComponents)
             {
-                case TrackedElement.Head:
-                    return participant.DeviceInfo?.IsHoloLens() ?? false
-                        ? CoreResourcesIDs.Prefabs.Networking.Participants.HoloLensHead_weprefab
-                        : CoreResourcesIDs.Prefabs.Networking.Participants.DefaultHead_weprefab;
-                case TrackedElement.LeftHand:
-                    return CoreResourcesIDs.Prefabs.Networking.Participants.leftPalm_weprefab;
-                case TrackedElement.RightHand:
-                    return CoreResourcesIDs.Prefabs.Networking.Participants.rightPalm_weprefab;
-                case TrackedElement.LeftController:
-                    return MRTKResourceIDs.Prefabs.DefaultLeftController_weprefab;
-                case TrackedElement.RightController:
-                    return MRTKResourceIDs.Prefabs.DefaultRightController_weprefab;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(element));
+                tint.TintColor = participant.AvatarColor;
             }
         }
     }
